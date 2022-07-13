7b521f3d12ed1c1ef38f90a30970908a965ee2a8
Maheshgoud2 <107980963+Maheshgoud2@users.noreply.github.com>
2022-07-13 16:08:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('😎Creater', url='https://t.me/Anmol0700')
+                InlineKeyboardButton('😎Creater', url='https://t.me/Spiner84')
             ],
             [
                 InlineKeyboardButton('😏 Jankari', url=f"https://t.me/{temp.U_NAME}?start=help"),
